c14beb70b7e16c0b63f7a738b651d1e247466e00
esanvee <eesanvee18@students.desu.edu>
2020-07-05 01:01:35 -0400

    Progress

--- src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
+++ src/test/java/hbcu/stay/ready/AnimalFactoryTest.java
@@ -1,7 +1,40 @@
 package hbcu.stay.ready;
 
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import hbcu.stay.ready.animals.Cat;
+import hbcu.stay.ready.animals.Dog;
+import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
 
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+
+    @Test
+
+    public void makeDogTest(){
+        Dog dog = AnimalFactory.createDog("Jim", new Date());
+
+        String expectedName = "Jim";
+        String actualName = dog.getName();
+
+        Assert.assertEquals(expectedName, actualName);
+
+    }
+
+    @Test
+
+    public void makeCatTest(){
+        Cat cat = AnimalFactory.createCat("Helen", new Date());
+
+        String expectedName = "Helen";
+        String actualName = cat.getName();
+
+        Assert.assertEquals(expectedName, actualName);
+
+    }
 }
