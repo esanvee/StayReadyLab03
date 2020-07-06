3ef448a6c33b5e52b4dc71b3a0827ae0d52ed9ec
esanvee <eesanvee18@students.desu.edu>
2020-07-06 01:02:11 -0400

    Ready for review

--- src/test/java/hbcu/stay/ready/DogHouseTest.java
+++ src/test/java/hbcu/stay/ready/DogHouseTest.java
@@ -1,6 +1,8 @@
 package hbcu.stay.ready;
 
 import hbcu.stay.ready.animals.animal_creation.AnimalFactory;
+
+import org.junit.Assert;
 import org.junit.Test;
 import hbcu.stay.ready.animals.Dog;
 import hbcu.stay.ready.animals.animal_storage.DogHouse;
@@ -28,5 +30,91 @@ public class DogHouseTest {
 
         // Then
         DogHouse.getNumberOfDogs();
+    }
+
+    @Test
+
+    public void addDogTest(){
+
+        Dog dog1 = new Dog("Scooby", new Date(), 12345);
+        
+        DogHouse.add(dog1);
+        int expectedValue = 1;
+        int actualValue = DogHouse.getNumberOfDogs();
+
+        Assert.assertEquals(expectedValue, actualValue);
+        
+
+    }
+
+    @Test
+
+    public void removeDogTest(){
+
+        Dog dog1 = new Dog("ScoobyDoo", new Date(), 737);
+        Dog dog2 = new Dog("dog", new Date(), 324);
+        
+        DogHouse.add(dog1);
+        DogHouse.add(dog2);
+        DogHouse.remove(737);
+
+        int expectedValue = 1;
+        int actualValue = DogHouse.getNumberOfDogs();
+
+        Assert.assertEquals(expectedValue, actualValue);
+        
+
+    }
+
+    @Test
+
+    public void removeDogTest2(){
+
+        Dog dog1 = new Dog("kdlsfad", new Date(), 10);
+        
+        DogHouse.add(dog1);
+        DogHouse.remove(dog1);
+        int expectedValue = 0;
+        int actualValue = DogHouse.getNumberOfDogs();
+
+        Assert.assertEquals(expectedValue, actualValue);
+        
+
+    }
+
+    @Test
+
+    public void getIdTest(){
+
+        Dog dog1 = new Dog("albert", new Date(), 77);
+        Dog dog2 = new Dog("dogboi", new Date(), 231);
+        
+        DogHouse.add(dog1);
+        DogHouse.add(dog2);
+        
+        Dog expectedDog = dog1;
+        Dog actualDog = DogHouse.getDogById(77);
+
+        Assert.assertEquals(expectedDog, actualDog);
+        
+
+    }
+
+    @Test
+
+    public void getTotalTest(){
+
+        Dog dog1 = new Dog("peepeepoopoo", new Date(), 707);
+        Dog dog2 = new Dog("dogboi", new Date(), 239);
+        
+        DogHouse.add(dog1);
+        DogHouse.add(dog2);
+        
+        int expectedDog = 2;
+        int actualDog = DogHouse.getNumberOfDogs();
+
+        Assert.assertEquals(expectedDog, actualDog);
+        
+
     }
 }
